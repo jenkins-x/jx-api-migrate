21898aa8a163eda9d8e4ba7b99a327fa7f6a63e9
James Rawlings <rawlingsj80@gmail.com>
2020-12-02 18:18:54 +0000

    tidy

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/jenkins-x/jx-api-migrate
 go 1.15
 
 require (
-	github.com/jenkins-x/jx-api/v4 v4.0.9
+	github.com/jenkins-x/jx-api/v4 v4.0.10
 	github.com/jenkins-x/jx-logging v0.0.11
 	github.com/stretchr/testify v1.6.1
 )
--- main.go
+++ main.go
@@ -66,6 +66,7 @@ func visit(path string, fi os.FileInfo, err error) error {
 		newContents := strings.Replace(string(read), "github.com/jenkins-x/jx-api/v3", "github.com/jenkins-x/jx-api/v4", -1)
 		newContents = strings.Replace(newContents, "github.com/jenkins-x/jx-api/v4/pkg/apis/jenkins.io/v1", "github.com/jenkins-x/jx-api/v4/pkg/apis/core/v4beta1", -1)
 		newContents = strings.Replace(newContents, "github.com/jenkins-x/jx-api/v4/pkg/apis/jenkins.io", "github.com/jenkins-x/jx-api/v4/pkg/apis/core", -1)
+		newContents = strings.Replace(newContents, "github.com/jenkins-x/jx-api/v4/pkg/client/clientset/versioned/typed/jenkins.io/v1", "github.com/jenkins-x/jx-api/v4/pkg/client/clientset/versioned/typed/core/v4beta1", -1)
 		newContents = strings.Replace(newContents, "\"github.com/jenkins-x/jx-api/v4/pkg/config\"", "jxcore \"github.com/jenkins-x/jx-api/v4/pkg/apis/core/v4beta1\"", -1)
 		newContents = strings.Replace(newContents, "config.RequirementsConfig", "jxcore.RequirementsConfig", -1)
 		newContents = strings.Replace(newContents, "jxconfig.LoadRequirementsConfig", "jxcore.LoadRequirementsConfig", -1)
@@ -74,6 +75,13 @@ func visit(path string, fi os.FileInfo, err error) error {
 		newContents = strings.Replace(newContents, "config.NewRequirementsConfig", "jxcore.NewRequirementsConfig", -1)
 		newContents = strings.Replace(newContents, ".JenkinsV1()", ".CoreV4beta1()", -1)
 		newContents = strings.Replace(newContents, "\"github.com/jenkins-x/jx-api/pkg/config\"", "jxcore \"github.com/jenkins-x/jx-api/v4/pkg/apis/core/v4beta1\"", -1)
+
+		// lets go back to v1 for anything other than requirements
+		newContents = strings.Replace(newContents, "github.com/jenkins-x/jx-api/v4/pkg/apis/core/v4beta1", "github.com/jenkins-x/jx-api/v4/pkg/apis/jenkins.io/v1", -1)
+		newContents = strings.Replace(newContents, "github.com/jenkins-x/jx-api/v4/pkg/apis/core", "github.com/jenkins-x/jx-api/v4/pkg/apis/jenkins.io", -1)
+		newContents = strings.Replace(newContents, "github.com/jenkins-x/jx-api/v4/pkg/client/clientset/versioned/typed/core/v4beta1", "github.com/jenkins-x/jx-api/v4/pkg/client/clientset/versioned/typed/jenkins.io/v1", -1)
+		newContents = strings.Replace(newContents, ".CoreV4beta1()", ".JenkinsV1()", -1)
+		newContents = strings.Replace(newContents, "jxcore \"github.com/jenkins-x/jx-api/v4/pkg/apis/jenkins.io/v1\"", "jxcore \"github.com/jenkins-x/jx-api/v4/pkg/apis/core/v4beta1\"", -1)
 
 		err = ioutil.WriteFile(path, []byte(newContents), 0)
 		if err != nil {
@@ -95,7 +103,7 @@ func visitRequirements(path string, fi os.FileInfo, err error) error {
 	}
 
 	if fi.Name() == "jx-requirements.yml" {
-		reqs, err := jxcore.LoadRequirementsConfigFile(path, false)
+		reqs, err := jxcore.LoadRequirementsConfigFileNoDefaults(path, false)
 		if err != nil {
 			panic(err)
 		}
